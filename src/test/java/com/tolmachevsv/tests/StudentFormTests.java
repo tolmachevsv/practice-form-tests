8362e8721ac69a544b3105402edc1e03dce065a8
Sergey <tolmachevsv96@yandex.ru>
2022-11-19 18:30:50 +0300

    added some changes

--- src/test/java/com/tolmachevsv/tests/StudentFormTests.java
+++ src/test/java/com/tolmachevsv/tests/StudentFormTests.java
@@ -1,4 +1,4 @@
-package guru.qa.tests;
+package com.tolmachevsv.tests;
 
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeAll;
@@ -24,59 +24,27 @@ public class StudentFormTests {
 
     @Test
     void fillFormTest() {
-        // enter first name
+
         $("#firstName").setValue("Billy");
-
-        // enter last name
         $("#lastName").setValue("Gymov");
-
-        // enter user's email
         $("input#userEmail").setValue("BillyGymov@ga.org");
-
-        // choose a random radiobutton
         $(by("for", "gender-radio-3")).click();
-
-        // enter user's mobile number
         $("#userNumber").setValue("88005553535");
-
-        // click on field of user's birthday
         $("#dateOfBirthInput").click();
-
-        // choose a random date
         $(".react-datepicker__month-select").selectOption("February");
         $(".react-datepicker__year-select").selectOption("1996");
         $(by("aria-label", "Choose Sunday, January 28th, 1996")).click();
-
-        // enter subject
         $("#subjectsInput").setValue("Math").pressEnter();
-
-        // choose the checkbox №2
         $(by("for", "hobbies-checkbox-2")).click();
-
-        // choose the checkbox №3
         $(by("for", "hobbies-checkbox-3")).click();
-
-        // upload a random file
         $("#uploadPicture").uploadFile(new File("src/test/data/300.png"));
-
-        // verify successful upload (can't find name of downloaded file in devtools)
-        // $(".form-file").shouldHave(text("300.png"));
-
         $("#currentAddress").setValue("300, Gatchino street, Saint-Peterburg");
-
-        // choose a random state
         $("#state").click();
         $("#stateCity-wrapper").$(byText("Haryana")).click();
-
-        // choose a random city
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Karnal")).click();
-
-        // press the button "Submit"
         $("#submit").click();
-
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-
         $(".table-responsive").shouldHave(
                 text("Billy Gymov"),
                 text("BillyGymov@ga.org"),
@@ -89,8 +57,6 @@ public class StudentFormTests {
                 text("300, Gatchino street, Saint-Peterburg"),
                 text("Haryana Karnal")
                 );
-
         $("#closeLargeModal").click();
-        sleep(2000);
     }
 }
